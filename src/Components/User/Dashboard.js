d3b025b2b1b5d2f7844af5b458b2a18ed5b76728
soueip <mohamedsouei11@gmail.com>
2024-04-18 23:45:22 +0100

    ss

--- src/Components/User/Dashboard.js
+++ src/Components/User/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Dashcompcard from './userwidget/Dashcompcard';
 import Dashshowcard from './userwidget/Dashshowcard';
 import './css/Dashboard.css';
+import Dashsmallcard from './userwidget/Dashsmallcard';
+import Dashsmallcard2 from './userwidget/Dashsmallcard2';
 
 const Dashboard = () => {
   return (
@@ -12,6 +14,15 @@ const Dashboard = () => {
       <div className="dashboardItem">
         <Dashshowcard shows={0} />
       </div>
+      <div className="dashboardItem">
+        <Dashsmallcard total={0} connected={0} notConnected={0} online={0} />
+        <Dashsmallcard total={0} connected={0} notConnected={0} online={0} />
+      </div>
+      <div className="dashboardithems">
+        <Dashsmallcard2 total={0} connected={0} notConnected={0} online={0} />
+        <Dashsmallcard2 total={0} connected={0} notConnected={0} online={0} />
+      </div>
+
     </div>
   );
 };
---
+++ src/Components/User/userwidget/Dashsmallcard2.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../css/widget/Dashsmallcard.css'
+const Dashsmallcard2 = ({ total, connected, notConnected,online }) => {
+  return (
+   
+      <div className="device-info-containerbig ">
+      <div className="device-info-content">
+        <div className='picturediv'> 
+        <img src="../img/user/devices.png " alt="connected" className="device-info-img" />
+        </div>
+        <div className='device-info-text'>
+        <h3>Devices Total: {total}</h3>
+        <p className='device-info-p'>Connected: {connected}</p>
+        <p className='device-info-p'>Not connected: {notConnected}</p>
+        <p className='device-info-p'>Online: {online}</p>
+        </div>
+      </div>
+      <a href="/n/#/devs" target="_self">
+        <button className="manage-button ">Manage</button>
+      </a>
+      </div>
+   
+  )
+}
+
+export default Dashsmallcard2
